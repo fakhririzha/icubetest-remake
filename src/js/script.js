d736c57df169eb506cc42435618d234bd761c2d6
Fakhri Rizha Ananda <mr.fakhririzhaa@gmail.com>
2021-10-04 12:45:28 +0700

    Finishing playlist section, YAY!

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,27 @@
 $(document).ready(function () {
-	$(".owl-carousel").owlCarousel({
+	$(".genre__list.owl-carousel").owlCarousel({
 		loop: true,
 		margin: 40,
 		dots: false,
 		nav: true,
-		navText: ["<", ">"],
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1,
+			},
+			767: {
+				items: 3,
+			},
+			768: {
+				items: 4,
+			},
+		},
+	});
+	$(".playlist__list.owl-carousel").owlCarousel({
+		loop: true,
+		margin: 20,
+		dots: false,
+		nav: true,
 		responsiveClass: true,
 		responsive: {
 			0: {
